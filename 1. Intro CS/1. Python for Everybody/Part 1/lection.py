da809325385baa98aaa144b9139318c5195418ce
djani_e <djani2703@gmail.com>
2020-11-03 14:28:54 +0200

    PY4E: Added program hello world and materials from the lecture (chpt 1)

---
+++ 1. Intro CS/1. Python for Everybody/Part 1/lection.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Sequential step:
+x = 2
+x = x + 2
+print(x)
+
+# Conditional step:
+x = 5
+if x < 10:
+    print(f'{x} smaller than 10..')
+if x > 20:
+    print(f'{x} bigger than 20!')
+print('Finish')
+
+# Repeated step:
+n = 5
+while n > 0:
+    print(n)
+    n = n -1
+print('Blastoff!')
