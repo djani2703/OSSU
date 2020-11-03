787fd681218b47d23781d92c4d81ef54895580f6
djani_e <djani2703@gmail.com>
2020-11-03 14:33:23 +0200

    PY4E: Worked with variables, expressions and statements in Python 3 (chpt 2)

---
+++ 1. Intro CS/1. Python for Everybody/Part 2/exercises.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Exercise 2:
+user_name = input('Enter your name: ')
+print('Hello', user_name)
+
+# Exercise 3:
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+pay = float(hours) * float(rate)
+print('Pay:', pay) 
---
+++ 1. Intro CS/1. Python for Everybody/Part 2/lection.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+input_data = input('Europe floor?: ')
+us_floor = int(input_data) + 1
+print('US floor:', us_floor)
